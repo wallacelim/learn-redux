0cad18cd9be9c57c564c401bd9e0ff19a95b5738
Wallace <wallacelim97@gmail.com>
2020-02-25 03:23:12 +0800

    Remove stub statuses + Fix bug where incorrect task is removed

--- src/App.js
+++ src/App.js
@@ -1,108 +1,74 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import styled from 'styled-components'
 import Table from 'react-bootstrap/Table'
+import Overlay from 'react-bootstrap/Overlay'
+import Tooltip from 'react-bootstrap/Tooltip'
 import './App.css';
 
 function App() {
 
-    const AppContainer = styled.div`
-        background: #282A36;
-        color: #BD93F9;
-        min-height: 100vh;
-    `
-
-    const Row = styled.div`
-        display: flex;
-        min-width: 100vw;
-        align-items: center;
-        justify-content: center;
-    `
-
-    const Active = styled.text`
-        color: #BD93F9;
-        background: #50FA7B;
-    `
-
-    const Button = styled.button`
-        background: ${props => props.active ? "#BD93F9" : "inherit"};
-        color: ${props => props.active ? "inherit" : "#BD93F9"};
-        &:hover {
-            background: #BD93F9;
-            color: #282A36;
-        }
-        font-size: 1em;
-        margin: 1em;
-        padding: 0.25em 1em;
-        border: 2px solid #BD93F9;
-        border-radius: 3px;
-    `
-
-    const Input = styled.input`
-        font-size: 1em;
-        margin: 1em;
-        padding: 0.25em;
-        border: 2px solid #BD93F9;
-        border-radius: 3px;
-    `
-
-    const StyledTable = styled(Table)`
-        text-align: left;
-        font-size: 1em;
-        margin: 1em;
-        padding: 0.25em;
-        border: 2px solid #BD93F9;
-        border-radius: 3px;
-        min-width: 40vw;
-    `
-
-    const TableRow = styled.tr`
-        margin: 1em;
-        padding: 0.25em;
-        border: 2px solid #BD93F9;
-        border-radius: 3px;
-    `
-
-    const TableHeader = styled.th`
-        padding: 1em 3em;
-        border-bottom: 2px solid #BD93F9;
-        text-align: center;
-    `
-
-    const TableHead = styled.thead`
-        border: 2px solid #BD93F9;
-        color: #50FA7B;
-    `
-
-    const TableDesc = styled.td`
-        padding: 1em 3em;
-        text-align: center;
-        color: #50FA7B;
-    `
-
+    // Event Handlers
     const handleAdd = () => {
-        let date = new Date()
-        let entry = {
-            dateAdded: `${date.getDate()}/${date.getMonth()}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}`,
-            taskName: document.getElementById('taskInput').value,
-            status: "in-progress"
+        let taskName = document.getElementById('taskInput').value;
+        if (!taskName) {
+            setShow(true);
+            setTimeout(() => {
+                setShow(false);
+            }, 1800);
+            return;
         }
-        setEntries([...entries, entry])
+        let date = new Date();
+        let entry = {
+            dateAdded: `${date.getDate().toString().padStart(2, '0')}/${date.getMonth().toString().padStart(2, '0')}/${date.getFullYear().toString().padStart(4, '0')} ${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`,
+            taskName: document.getElementById('taskInput').value,
+            status: "IN-PROGRESS"
+        };
+        setEntries([...entries, entry]);
     }
 
     const handleRemove = e => {
-        let toRemove = e.target.getAttribute("name")
-        console.log(toRemove)
-        setEntries(entries.filter((entry) => entry != toRemove))
+        let toRemove = e.target.getAttribute("name");
+        setEntries([
+            ...entries.slice(0, toRemove),
+            ...entries.slice(++toRemove)
+        ]);
     }
 
-    const [entries, setEntries] = useState([])
+    const handleStatusToggle = e => {
+        let toToggle = e.target.getAttribute("name");
+        let updatedEntry = entries[toToggle]
+        if (entries[toToggle].status === "IN-PROGRESS") {
+            updatedEntry.status = "COMPLETE";
+        } else if (entries[toToggle].status === "COMPLETE") {
+            updatedEntry.status = "IN-PROGRESS";
+        }
+        let newEntries = [
+            ...entries.slice(0, toToggle),
+            updatedEntry,
+            ...entries.slice(++toToggle)
+        ]
+        setEntries([...newEntries]);
+    }
+
+    // State hooks
+    const [entries, setEntries] = useState([]);
+    const [show, setShow] = useState(false);
+
+    // Ref hooks
+    const target = useRef(null);
 
     return (
         <AppContainer className="App">
             <Row>
                 <Input id="taskInput" />
-                <Button onClick={handleAdd
-                }>Add</Button>
+                <Button ref={target} onClick={handleAdd}>Add</Button>
+                <Overlay target={target.current} show={show} placement="right">
+                    {props => (
+                        <StyledTooltip arrowProps={target} id="overlay-example" {...props}>
+                            Please input a task name
+                        </StyledTooltip>
+                    )}
+                </Overlay>
             </Row>
             <Row>
                 <StyledTable striped bordered hover>
@@ -115,13 +81,15 @@ function App() {
                         </TableRow>
                     </TableHead>
                     <tbody>
-                        {entries.map(entry =>
-                            <TableRow>
+                        {entries && entries.map((entry, idx) =>
+                            <TableRow key={idx}>
                                 <TableDesc>{entry.dateAdded}</TableDesc>
                                 <TableDesc>{entry.taskName}</TableDesc>
-                                <TableDesc>{entry.status}</TableDesc>
                                 <TableDesc>
-                                    <Button name={entry} onClick={handleRemove}>Remove</Button>
+                                    <ProgressButton name={idx} onClick={handleStatusToggle}> {entry.status} </ProgressButton>
+                                </TableDesc>
+                                <TableDesc>
+                                    <RemoveButton name={idx} onClick={handleRemove}>Remove</RemoveButton>
                                 </TableDesc>
                             </TableRow>
                         )}
@@ -131,5 +99,104 @@ function App() {
         </AppContainer>
     );
 }
+
+// Styled Components
+const AppContainer = styled.div`
+        background: #282A36;
+        color: #BD93F9;
+        min-height: 100vh;
+    `
+
+const Row = styled.div`
+        display: flex;
+        min-width: 100vw;
+        align-items: center;
+        justify-content: center;
+    `
+
+const Button = styled.button`
+        background: ${props => props.active ? "#BD93F9" : "inherit"};
+        color: ${props => props.complete ? "inherit" : "#BD93F9"};
+        &:hover {
+            background: #BD93F9;
+            color: #282A36;
+        }
+        &:focus {
+            outline: none;
+        }
+        font-size: 1em;
+        margin: 1em;
+        padding: 0.25em 1em;
+        border: 2px solid #BD93F9;
+        border-radius: 3px;
+    `
+
+const RemoveButton = styled(Button)`
+    color: #FA8072;
+    border: 2px solid #FA8072;
+    &:hover {
+            background: #FA8072;
+            color: #282A36;
+        }
+    `
+
+const ProgressButton = styled(Button)`
+    color: #50FA7B;
+    border: 2px solid #50FA7B;
+    &:hover {
+            background: #50FA7B;
+            color: #282A36;
+        }
+`
+
+const Input = styled.input`
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em;
+    border: 2px solid #BD93F9;
+    border-radius: 3px;
+`
+
+const StyledTable = styled(Table)`
+        text-align: left;
+        font-size: 1em;
+        margin: 1em;
+        padding: 0.25em;
+        border: 2px solid #BD93F9;
+        border-radius: 3px;
+        min-width: 40vw;
+    `
+
+const TableRow = styled.tr`
+        margin: 1em;
+        padding: 0.25em;
+        border: 2px solid #BD93F9;
+        border-radius: 3px;
+    `
+
+const TableHeader = styled.th`
+        padding: 1em 3em;
+        border-bottom: 2px solid #BD93F9;
+        text-align: center;
+    `
+
+const TableHead = styled.thead`
+        border: 2px solid #BD93F9;
+        color: #50FA7B;
+    `
+
+const TableDesc = styled.td`
+        padding: 1em 3em;
+        text-align: center;
+        color: #50FA7B;
+    `
+
+const StyledTooltip = styled(Tooltip)`
+    background: #50FA7B;
+    padding: 0.25em 0.5em;
+    margin-left: 5px;
+    border-radius: 3px;
+`
+
 
 export default App;
